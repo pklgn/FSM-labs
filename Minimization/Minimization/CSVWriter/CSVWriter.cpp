96b367184c33d24d303cd0d194a10f4a0de6af1c
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-10-25 01:43:49 +0300

    [lab2] add csv writer

--- Minimization/Minimization/CSVWriter/CSVWriter.cpp
+++ Minimization/Minimization/CSVWriter/CSVWriter.cpp
@@ -1,9 +1,71 @@
 #include "CSVWriter.h"
 
-void CSVWriter::WriteMealyTable(const MealyTable&)
+const char CSV_DELIMETER = ';';
+const char MEALY_STATE_DELIMETER = '/';
+
+const char STATE_PREFIX = 'q';
+const char OUTPUT_SIGNAL_PREFIX = 'w';
+
+CSVWriter::CSVWriter(const std::string& outputFileName)
+	: m_outputStream(outputFileName)
 {
 }
 
-void CSVWriter::WriteMooreTable(const MooreTable&)
+void CSVWriter::WriteMealyTable(const MealyTable& mealyTable)
 {
+	WriteStatesHeader(mealyTable.GetStates());
+
+	auto inputSignals = mealyTable.GetInputSignals();
+	auto lineCount = inputSignals.size();
+	auto transitionTable = mealyTable.GetTransitionTable();
+	for (auto line = 0; line < lineCount; ++line)
+	{
+		m_outputStream << inputSignals[line];
+		for (auto& [state, transitions] : transitionTable)
+		{
+			m_outputStream << CSV_DELIMETER
+						   << STATE_PREFIX << transitions.commonStates[line]
+						   << MEALY_STATE_DELIMETER
+						   << OUTPUT_SIGNAL_PREFIX << transitions.outputSignals[line];
+		}
+		m_outputStream << std::endl;
+	}
+}
+
+void CSVWriter::WriteMooreTable(const MooreTable& mooreTable)
+{
+	WriteMooreOutputSignalsHeader(mooreTable.GetOutputSignals());
+	WriteStatesHeader(mooreTable.GetStates());
+
+	auto inputSignals = mooreTable.GetInputSignals();
+	auto lineCount = inputSignals.size();
+	auto transitionTable = mooreTable.GetTransitionTable();
+	for (auto line = 0; line < lineCount; ++line)
+	{
+		m_outputStream << inputSignals[line];
+		for (auto& [state, transitions] : transitionTable)
+		{
+			m_outputStream << CSV_DELIMETER
+						   << STATE_PREFIX << transitions.commonStates[line];
+		}
+		m_outputStream << std::endl;
+	}
+}
+
+void CSVWriter::WriteStatesHeader(const States& states)
+{
+	for (auto& state : states)
+	{
+		m_outputStream << CSV_DELIMETER << STATE_PREFIX + state;
+	}
+	m_outputStream << std::endl;
+}
+
+void CSVWriter::WriteMooreOutputSignalsHeader(const Signals& outputSignals)
+{
+	for (auto& signal : outputSignals)
+	{
+		m_outputStream << CSV_DELIMETER << OUTPUT_SIGNAL_PREFIX << signal;
+	}
+	m_outputStream << std::endl;
 }
--- Minimization/Minimization/CSVWriter/CSVWriter.h
+++ Minimization/Minimization/CSVWriter/CSVWriter.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <string>
+#include <fstream>
+#include "../Tables/FSMTable.hpp"
 #include "../Tables/MealyTable.h"
 #include "../Tables/MooreTable.h"
 
 class CSVWriter
 {
+public:
 	CSVWriter(const std::string& outputFileName);
 
 	void WriteMealyTable(const MealyTable&);
 
 	void WriteMooreTable(const MooreTable&);
+
+protected:
+	void WriteStatesHeader(const States&);
+	void WriteMooreOutputSignalsHeader(const Signals&);
+
+	std::ofstream m_outputStream;
 };
