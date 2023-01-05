4335ee00a2e70a3f0a84bedbc446a2aeb2c510e5
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-01-05 19:14:18 +0300

    [lab3] add printing empty state with "-" string

--- Determination/Determination/CSVWriter/CSVWriter.cpp
+++ Determination/Determination/CSVWriter/CSVWriter.cpp
@@ -3,6 +3,7 @@
 
 const char CSV_DELIMITER = ';';
 const char MEALY_STATE_DELIMITER = '/';
+const char NO_STATE_STR = '-';
 
 const std::string CSVWriter::STATE_PREFIX = "q";
 
@@ -25,9 +26,14 @@ void CSVWriter::WriteMealyTable(const MealyTable& mealyTable, const std::string&
 		m_outputStream << inputSignals[line];
 		for (auto& state: states)
 		{
-			auto&& currStates = transitionTable[state].commonStates[line];
+			auto currStates = transitionTable[state].commonStates[line];
+			auto currStatesStr = std::accumulate(currStates.begin(), currStates.end(), std::string(""));
+			if (currStatesStr.empty())
+			{
+				currStatesStr = NO_STATE_STR;
+			}
 			m_outputStream << CSV_DELIMITER
-						   << prefix << std::accumulate(currStates.begin(), currStates.end(), std::string(""))
+						   << prefix << currStatesStr
 						   << MEALY_STATE_DELIMITER
 						   << transitionTable[state].outputSignals[line];
 		}
@@ -50,9 +56,14 @@ void CSVWriter::WriteMooreTable(const MooreTable& mooreTable, const std::string&
 		m_outputStream << inputSignals[line];
 		for (auto& state: states)
 		{
-			auto&& currStates = transitionTable[state].commonStates[line];
+			auto currStates = transitionTable[state].commonStates[line];
+			auto currStatesStr = std::accumulate(currStates.begin(), currStates.end(), std::string(""));
+			if (currStatesStr.empty())
+			{
+				currStatesStr = NO_STATE_STR;
+			}
 			m_outputStream << CSV_DELIMITER
-						   << prefix << std::accumulate(currStates.begin(), currStates.end(), std::string(""));
+						   << prefix << currStatesStr;
 		}
 		m_outputStream << std::endl;
 	}
