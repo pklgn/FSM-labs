3443c2c86f5a28c6d5f1707657092dc0c0a7c27f
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-01-20 13:55:26 +0300

    [lab5] add console template

---
+++ Lexer/Console_Lexer/Console_Lexer.cpp
@@ -0,0 +1,34 @@
+// Console_Lexer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "../Lexer/Lexer/Lexer.h"
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc == 2)
+		{
+			std::ifstream inputFile(argv[1]);
+			if (!inputFile)
+			{
+				throw std::runtime_error("Unable to open file");
+			}
+			Lexer lexer;
+			lexer.Run(inputFile);
+		}
+		else
+		{
+			std::cout << "Unexpected amount of input params\n";
+			return 1;
+		}
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what();
+
+		return 1;
+	}
+
+}
